e17af9d3ee569d44829dd6949a893b59131a4a85
Patricia Pineda <pepineda28@gmail.com>
2023-05-22 02:27:39 +0800

    def subtract

--- calculator.py
+++ calculator.py
@@ -2,3 +2,7 @@ class Calculator:
     def add(self, num1, num2):
         sum = num1 + num2
         return sum
+    
+    def subtract(self, num1, num2):
+        difference = num1 + num2
+        return difference
