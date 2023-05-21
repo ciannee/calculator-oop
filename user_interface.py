34d0091d8c47faf369f84821c5e24c2728e30f20
Patricia Pineda <pepineda28@gmail.com>
2023-05-22 02:01:34 +0800

    ask user for operation

--- user_interface.py
+++ user_interface.py
@@ -1,1 +1,5 @@
 class UserInterface:
+    def ask_input(self):
+       operation = input("\n\033[01m\nPlease choose among the four math operations:\n\nAddition\nSubtraction\nMultiplication\nDivision\n\nEnter your answer: ")
+       return operation
+    
