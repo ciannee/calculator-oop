8fe5f1c74036d528377287c7cde3b0516c6e3112
Patricia Pineda <pepineda28@gmail.com>
2023-05-22 02:11:57 +0800

    create num object

--- user_interface.py
+++ user_interface.py
@@ -2,4 +2,7 @@ class UserInterface:
     def ask_input(self):
        operation = input("\n\033[01m\nPlease choose among the four math operations:\n\nAddition\nSubtraction\nMultiplication\nDivision\n\nEnter your answer: ")
        return operation
+
+       num = float(input("Input a number: "))
+       return num
     
