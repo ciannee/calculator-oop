cb64be921ff3e24e6f4b853960a109426dc4358c
Patricia Pineda <pepineda28@gmail.com>
2023-05-22 01:56:01 +0800

    pseudocode

--- program_oop.py
+++ program_oop.py
@@ -1,3 +1,12 @@
 # Name: Pineda, Patricia Anne E.
 # Section: BSCPE 1-5
 # Assignment 7: Redo calculator using OOP concept
+
+# Pseudocode
+
+# ask user for input (math operations)
+# ask user for number input
+# addition
+# subtraction
+# multiplication
+# division
